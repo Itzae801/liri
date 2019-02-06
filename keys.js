5009d4a53231746f705f2e36c106e66a35797f95
Itzae <itzae801@gmail.com>
2019-02-05 20:15:16 -0700

    1

--- keys.js
+++ keys.js
@@ -1,10 +1,4 @@
-console.log('this is loaded');
-
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
-
-exports.ombd = {
-    id: process.env.OMBD}
--- liri.js
+++ liri.js
@@ -1,94 +1,120 @@
-//grab data from api keys/ 
 require("dotenv").config();
 
-var Spotify = require('node-spotify-api');
+var request = require("request");
 var fs = require("fs");
-var keys = require('./keys.js');
-var request = require('request');
+var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var moment = require("moment");
 
-var api_key = process.env.API_KEY;
-var spotify = new Spotify(keys.spotify);
+var arguments = process.argv;
 
+var command = arguments[2].toLowerCase();
 
+var cmdArg = arguments.slice(3).join(" ");
 
-//stored arguments arrayvar nodeArgv = process.argv;'
-var nodeArgv = process.argv;
-var command = process.argv[2];
-//movie or song
-var x = "";
-//attaches multiple word arguments
-for (var i=3; i<nodeArgv.length; i++){
-  if(i>3 && i<nodeArgv.length){
-    x = x + "+" + nodeArgv[i];
-  } else{
-    x = x + nodeArgv[i];
-  }
+function spotifyThis(cmdArg) {
+
+  var spotify = new Spotify(keys.spotify);
+
+  spotify.search({ type: 'track', query: cmdArg }, function(err, data) {
+    if (err) {
+      return logIt('Error occurred: ' + err);
+    }
+   
+  logIt("Artist: " + data.tracks.items[0].artists[0].name);
+  logIt("Track Name: " + data.tracks.items[0].name); 
+  logIt("Preview URL: " + data.tracks.items[0].preview_url); 
+  logIt("Album: " + data.tracks.items[0].album.name);
+  });
 }
 
+function concertThis(cmdArg) {
+  request("https://rest.bandsintown.com/artists/" + cmdArg + "/events?app_id=codingbootcamp", function(error, response, body) {
 
-
-fs.readFile("random.txt", "utf8", function(error, data) {
-
- 
-    if (error) {
-      console.log(error);
-    }
-  
-
-    var output = data.slice(",");
-  
-    for (var x = 0; x < output.length;x++){
-        console.log(output[x]);
+    // If the request is successful (i.e. if the response status code is 200)
+    if (!error && response.statusCode === 200) {
+      var JS = JSON.parse(body);
+      if (JS.venue !== "") {
+        logIt("Venue: " + JS[0].venue.name);
+        logIt("Location: " + JS[0].venue.city + ", " + JS[0].venue.region + ", " + JS[0].venue.country);
+        logIt("Date & Time: " + moment(JS[0].datetime).format("MM/DD/YYYY HH:mm"));
+      } else {
+        logIt("No results found");
+      }
     }
   });
-  switch(command){
-  case "spotify-this-song":
-  if(x){
-    spotifySong(x);
-  } else{
-    spotifySong("Fluorescent Adolescent");
-  }
-break;
+}
 
-default:
-console.log("{Please enter a command: spotify-this-song, movie-this, do-what-it-says}");
-break;
-  }
+function movieThis(cmdArg) {
+  request("http://www.omdbapi.com/?apikey=trilogy&t=" + cmdArg, function(error, response, body) {
 
-  function spotifySong(song){
-    spotify.search({ type: 'track', query: song}, function(error, data){
-      if(!error){
-        for(var i = 0; i < data.tracks.items.length; i++){
-          var songData = data.tracks.items[i];
-          //artist
-          console.log("Artist: " + songData.artists[0].name);
-          //song name
-          console.log("Song: " + songData.name);
-          //spotify preview link
-          console.log("Preview URL: " + songData.preview_url);
-          //album name
-          console.log("Album: " + songData.album.name);
-          console.log("-----------------------");
-          
-          //adds text to log.txt
-          fs.appendFile('log.txt', songData.artists[0].name);
-          fs.appendFile('log.txt', songData.name);
-          fs.appendFile('log.txt', songData.preview_url);
-          fs.appendFile('log.txt', songData.album.name);
-          fs.appendFile('log.txt', "-----------------------");
-        }
-      } else{
-        console.log('Error occurred.');
-      }
+    // If the request is successful (i.e. if the response status code is 200)
+    if (!error && response.statusCode === 200) {
+
+      var result = JSON.parse(body);
+      logIt("Title: " + result.Title);
+      logIt("Released: " + result.Year);
+      logIt("IMDB Rating: " + result.Ratings[0].Value);
+      logIt("Rotten Tomatoes Rating: " + result.Ratings[1]);
+      logIt("Country: " + result.Country);
+      logIt("Language: " + result.Language);
+      logIt("Plot: " + result.Plot);
+      logIt("Actors: " + result.Actors);
+    }
+  });
+}
+
+function testCommands() {
+  if (command === "spotify-this-song") {
+    if (cmdArg) {
+      logIt("Command: " + command + ", Argument: " + cmdArg);
+      spotifyThis(cmdArg);
+    } else {
+      cmdArg = "The Sign Ace Of Base";
+      logIt("Command: " + command + ", Argument: " + cmdArg);
+      spotifyThis(cmdArg);
+    };
+  } else if (command === "concert-this") {
+    if (cmdArg) {
+      logIt("Command: " + command + ", Argument: " + cmdArg);
+      concertThis(cmdArg);
+    } else {
+      logIt("Command: " + command);
+      return logIt("Please provide a band name");
+    };
+  } else if (command === "movie-this") {
+    if (cmdArg) {
+      logIt("Command: " + command + ", Argument: " + cmdArg);
+      movieThis(cmdArg);
+    } else {
+      logIt("Command: " + command);
+      return logIt("Please provide a movie name");
+    };
+  } else if (command === "do-what-it-says") {
+    logIt("Command: " + command);
+    fs.readFile("random.txt", "utf8", function(error, data) {
+      if (error) {
+        return logIt(error);
+      }  
+      var dataArr = data.split(",");  
+      command = dataArr[0];
+      cmdArg = dataArr[1];
+      testCommands();  
     });
-  }
+  } else {
+    logIt("Command: " + command);
+    logIt("Please enter a valid command (ie. spotify-this-song, concert-this, movie-this, do-what-it-says");
+  };
+}
 
-  function doThing(){
-    fs.readFile('random.txt', "utf8", function(error, data){
-      var txt = data.slice(',');
-  
-      spotifySong(txt[1]);
-    });
-  }
+testCommands();
 
+function logIt(dataToLog) {
 
+	console.log(dataToLog);
+
+	fs.appendFile('log.txt', dataToLog + '\n', function(err) {
+		
+		if (err) return logIt('Error logging data to file: ' + err);	
+	});
+}
